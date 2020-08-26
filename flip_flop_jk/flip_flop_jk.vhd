f92037cb616f2f8d6f9796c22cd234db4b5b2ebb
yurnasg <yuri.argolo1@hotmail.com>
2020-08-26 12:10:18 -0300

    Up do FF JK

---
+++ flip_flop_jk/flip_flop_jk.vhd
@@ -0,0 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity FF_JK is port (  clock: in std_logic; 
+                        J, K: in std_logic;
+                        reset: in std_logic;
+                        Q, Qbar: out std_logic );
+end FF_JK;
+
+architecture dentro of FF_JK is
+
+   signal state: std_logic;
+   signal input: std_logic_vector(1 downto 0);
+begin
+
+   input <= J & K;		
+   p: process(clock, reset) is
+   begin
+    if (reset='1') then state <= '0';
+        elsif (rising_edge(clock)) then case (input) is when "11" => state <= not state;
+        when "10" => state <= '1';
+        when "01" => state <= '0';
+        when others => null;
+        end case;
+    end if;
+
+   end process;
+   -- Vi isso na net
+   Q <= state;
+   Qbar <= not state;
+end dentro;
